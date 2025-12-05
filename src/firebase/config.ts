e4213a8a882b95bb9c534cce4cbfab5644f38a7d
Mayank Banks <mightyshooter0@gmail.com>
2025-12-05 08:19:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7670154401-37d76",
+  "appId": "1:574231811704:web:6244816af53685a6c3ff8d",
+  "apiKey": "AIzaSyAsOCpjG6wgVubaiNXMAPnG_wMZeF5zZlU",
+  "authDomain": "studio-7670154401-37d76.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "574231811704"
+};
